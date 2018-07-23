a94278924cfa38f536251e95cc118dae04849899
Emilio <demouser@IT-BE-LIKE-THAT-SOMETIMES.local>
2018-07-23 09:39:49 -0700

    uptdated python

---
+++ main.py
@@ -0,0 +1,17 @@
+import jinja2
+import webapp2
+import os
+import json
+import urllib
+import urllib2
+from google.appengine.ext import ndb
+
+jinja_env = jinja2.Environment(
+    loader= jinja2.FileSystemLoader(
+        os.path.dirname(__file__) + '/templates'))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/recents', RecentsPage),
+    ('/popular', PopularPage)
+])
