216f96bea8f7c50f708c4339fe9f6f976f9b3207
Mikayla <demouser@MacBook-Pro.local>
2018-07-25 16:36:50 -0700

    main.py

--- main.py
+++ main.py
@@ -80,6 +80,88 @@ class UserSearch(ndb.Model):
     comments = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
     updated = ndb.DateTimeProperty(auto_now=True)
+    count = ndb.IntegerProperty(required=True)
+
+class SavedMethods(ndb.Model):
+    click1 = ndb.StringProperty(required=False)
+    click2 = ndb.StringProperty(required=False)
+    click3 = ndb.StringProperty(required=False)
+    click4 = ndb.StringProperty(required=False)
+    click5 = ndb.StringProperty(required=False)
+    click6 = ndb.StringProperty(required=False)
+    click7 = ndb.StringProperty(required=False)
+    click8 = ndb.StringProperty(required=False)
+    click9 = ndb.StringProperty(required=False)
+    click10 = ndb.StringProperty(required=False)
+    click11 = ndb.StringProperty(required=False)
+    click12 = ndb.StringProperty(required=False)
+    click13 = ndb.StringProperty(required=False)
+    click14 = ndb.StringProperty(required=False)
+    click15 = ndb.StringProperty(required=False)
+    click16 = ndb.StringProperty(required=False)
+    click17 = ndb.StringProperty(required=False)
+    click18 = ndb.StringProperty(required=False)
+
+
+def getSavedTips():
+    return SavedMethods.query().fetch()
+
+
+class SavedPage(webapp2.RequestHandler):
+    def get(self):
+        searches = getSavedTips()
+        template = jinja_env.get_template('templates/saved.html')
+        variables = {'recentsaves': searches}
+        print(searches)
+
+        self.response.write(template.render(variables))
+    def post(self):
+        my_dict = {
+            'click1': self.request.get('click1'),
+            'click2': self.request.get('click2'),
+            'click3': self.request.get('click3'),
+            'click4': self.request.get('click4'),
+            'click5': self.request.get('click5'),
+            'click6': self.request.get('click6'),
+            'click7': self.request.get('click7'),
+            'click8': self.request.get('click8'),
+            'click9': self.request.get('click9'),
+            'click10': self.request.get('click10'),
+            'click11': self.request.get('click11'),
+            'click12': self.request.get('click12'),
+            'click13': self.request.get('click13'),
+            'click14': self.request.get('click14'),
+            'click15': self.request.get('click15'),
+            'click16': self.request.get('click16'),
+            'click17': self.request.get('click17'),
+            'click18': self.request.get('click18')
+        }
+        story = SavedMethods(click1=my_dict["click1"],
+                            click2=my_dict["click2"],
+                            click3=my_dict["click3"],
+                            click4=my_dict["click4"],
+                            click5=my_dict["click5"],
+                            click6=my_dict["click6"],
+                            click7=my_dict["click7"],
+                            click8=my_dict["click8"],
+                            click9=my_dict["click9"],
+                            click10=my_dict["click10"],
+                            click11=my_dict["click11"],
+                            click12=my_dict["click12"],
+                            click13=my_dict["click13"],
+                            click14=my_dict["click14"],
+                            click15=my_dict["click15"],
+                            click16=my_dict["click16"],
+                            click17=my_dict["click17"],
+                            click18=my_dict["click18"])
+        tips_template = jinja_env.get_template("templates/tips.html")
+        story.put()
+        self.response.write(tips_template.render(my_dict))
+        saved_template = jinja_env.get_template('templates/saved.html')
+        recentsaves = SavedMethods.query().fetch()
+
+
+
 
 class ContactHandler(webapp2.RequestHandler):
     def get(self):
@@ -100,6 +182,7 @@ class ContactHandler(webapp2.RequestHandler):
         story.put()
         self.response.write(contact2_template.render(my_dict))
 
+
 class Contact3Handler(webapp2.RequestHandler):
     def get(self):
         contact3_template = jinja_env.get_template('contact3.html')
@@ -115,4 +198,5 @@ app = webapp2.WSGIApplication([
     ('/tips', TipsHandler),
     ('/contact', ContactHandler),
     ('/contact3', Contact3Handler),
+    ('/saved', SavedPage),
 ], debug=True)
